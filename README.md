47791353ef8c3fd9362150193a432dce12ff8a48
aepyornis <ziggy@elephant-bird.net>
2015-04-19 13:57:15 -0400

    php page, style changes, readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# row-memorial-2014
+# 2014 Row Memorial / Vision 264
+
+[Right of Way](http://www.rightofway.org) memorializes those who were killed by traffic in 2014. 
+
+View the page [here](http://www.rightofway.org/2014-memorial)
+
+### Technical details
+The folder wnyc-scrape contains the scripts used to generate the artwork. After we created this artwork, WNYC released their [mean-streets data](https://github.com/datanews/mean-streets) publicly, rendering the scraping unnecessary. 'row.jsx' is an in-design script. 
+
---
+++ memorial.php
@@ -0,0 +1,165 @@
+<?php
+/*
+Template Name: memorial
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>2014 Memorial</title>
+  <meta name="description" content="">
+  <meta name="author" content="Right of way">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/css/normalize.css">
+  <link rel="stylesheet" href="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/css/skeleton.css">
+  <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" />
+  <link rel="stylesheet" href="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/css/style.css">
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
+<body>
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <div class="section cover-image">
+    <div class="container">
+      <div class="row">
+        <div class="one-half column title">
+          <a href="http://www.rightofway.org"><h2>RIGHT OF WAY</h2></a>
+          <h2>2014 memorial</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="section facts">
+    <div class="container">
+      <div class="row">
+        <div class="one-third column">
+          <h2>269 People were killed by cars in NYC in 2014</h4>
+          <h3><b>144</b> Pedestrians</h3>
+          <h3><b>20</b> Cyclists</h3>
+          <h3><b>95</b> people in cars</h3>
+          <h3><b>10</b> unknown</h3>
+          <h5>Right of Way remembers them with a large public art installation</h5>
+        </div>
+        <div class="two-thirds column">
+          <h3 id="icons" class="icon-style"></h3>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="section photo-strip">
+    <div class="container">
+      <div class="row">
+        <div class="four columns">
+          <img class="u-max-full-width" src="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/images/big3/abonour_row_walloffatals03_sm.jpg"></img>
+        </div>
+        <div class="four columns">
+          <img class="u-max-full-width" src="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/images/big3/abonour_row_walloffatals05_sm.jpg"></img>
+        </div>
+        <div class="four columns">
+          <img class="u-max-full-width" src="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/images/big3/abonour_row_walloffatals16_sm.jpg"></img>
+        </div>
+      </div>
+      <div class ="row main-text">
+        <h4>On April 18th, Right of Way installed a memorial for 264 victims of traffic violence on Kent Avenue in Brooklyn. <a href="http://www.rightofway.org/campaigns/vision-264">Click here to read the full report</a>. 
+        </h4>
+      </div>
+    </div>
+  </div>
+
+  <div class="section photos">
+    <div class="container">
+      <div class="row">
+        <div class="three columns">
+          <img class="u-max-full-width" src="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/images/01.jpg"></img>
+        </div>
+        <div class="three columns">
+          <img class="u-max-full-width" src="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/images/02.jpg"></img>
+        </div>
+        <div class="three columns">
+          <img class="u-max-full-width" src="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/images/03.jpg"></img>
+        </div>
+        <div class="three columns">
+          <img class="u-max-full-width" src="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/images/04.jpg"></img>
+        </div>
+      </div>
+      <div class="row">
+        <div class="three columns">
+          <img class="u-max-full-width" src="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/images/zy_1.jpg"></img>
+        </div>
+        <div class="three columns">
+          <img class="u-max-full-width" src="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/images/05.jpg"></img>
+        </div>
+        <div class="three columns">
+          <img class="u-max-full-width" src="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/images/06.jpg"></img>
+        </div>
+        <div class="three columns">
+          <img class="u-max-full-width" src="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/images/07.jpg"></img>
+        </div>
+      </div>
+      <div class="row">
+        <div class="three columns">
+          <img class="u-max-full-width" src="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/images/zy_2.jpg"></img>
+        </div>
+        <div class="three columns">
+          <img class="u-max-full-width" src="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/images/08.jpg"></img>
+        </div>
+        <div class="three columns">
+          <img class="u-max-full-width" src="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/images/09.jpg"></img>
+        </div>
+        <div class="three columns">
+          <img class="u-max-full-width" src="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/images/10.jpg"></img>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="section the-map">
+    <div class="container">
+      <h3><center>Map of those killed in 2014</center></h3>
+      <div id="map"></div>
+    </div>
+  </div>
+
+ <div class="section">
+  <div class="container bottom-note">
+    <p>* After we created our artwork, the total deaths rose to 269 from 264.</p>
+    <p>Source code for the website and artwork <a href="https://github.com/aepyornis/row-memorial-2014">available here</a></p>
+  </div>
+</dvi>
+
+
+<script src="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
+<script src="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/js/deaths.js"></script>
+<script src="http://www.rightofway.org/wp-content/themes/rightofway/2014memorial/js/main.js"></script>
+
+
+</body>
+
+</html>
+
+
+
+
+
